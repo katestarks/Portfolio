a7c5ee854f60071a93a852e45d0dfe3da2051a85
katestarks <katestarks@me.com>
2019-03-15 16:45:15 +0000

    set up files to start admin page

---
+++ php/cms_page.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once 'dbConnection.php';
+
+?>
+
+<html lang="en">
+
+<head>
+    <title>Admin for Kate's Portfolio Page</title>
+    <link rel='stylesheet' type='text/css' href='../css/normalize.css'/>
+    <link href="https://fonts.googleapis.com/css?family=Quicksand:300,500" rel="stylesheet">
+    <link rel='stylesheet' type='text/css' href='../css/cms_styles.css'/>
+</head>
+
+<body>
+
+</body>
+</html>
