9e3c3abf97acef60852f03bbb7305ed72bab3180
katestarks <katestarks@me.com>
2019-03-18 15:35:46 +0000

    updated functions to exclude deleted content, added docblocks, added blank test php file

--- php/functions.php
+++ php/functions.php
@@ -2,13 +2,28 @@
 
 require_once "dbConnection.php";
 
-function getAboutMeText($db) {
-    $query = $db->prepare("SELECT `content` FROM `about_me` LIMIT 1,50");
+
+/**
+ * Retrieves content from the database excluding line 1
+ *
+ * @param PDO $db database of about me text
+ *
+ * returns an array of content field excluding the first line of the database
+ */
+function getAboutMeText(PDO $db) : array {
+    $query = $db->prepare("SELECT `content` FROM `about_me` WHERE `deleted` = 0 LIMIT 1,50");
     $query->execute();
     return $query->fetchAll();
 }
 
-function displayAboutMeText($aboutMeTexts) {
+/**
+ * Displays data held in the array fetched from the database which excludes line 1
+ *
+ * @param array $aboutMeTexts result of function to get the about me text (excl line 1) assigned to a variable
+ *
+ * returns string of the array items concatenated with html
+ */
+function displayAboutMeText(array $aboutMeTexts) : string {
     $result="";
     foreach ($aboutMeTexts as $aboutMeText) {
         $result .= "<p>" . $aboutMeText['content'] . "</p>";
@@ -16,13 +31,27 @@ function displayAboutMeText($aboutMeTexts) {
     return $result;
 }
 
-function getAboutMeQuote($db) {
-    $query = $db->prepare("SELECT `content` FROM `about_me` LIMIT 1");
+/**
+ * Retrieves content from the database limited to line one
+ *
+ * @param PDO $db database of about me text
+ *
+ * returns an array of content field limited to the first line of the database
+ */
+function getAboutMeQuote(PDO $db) : array {
+    $query = $db->prepare("SELECT `content` FROM `about_me` WHERE `deleted` = 0 LIMIT 1");
     $query->execute();
     return $query->fetchAll();
 }
 
-function displayAboutMeQuote($aboutMeQuotes) {
+/**
+ * Displays data held in the array fetched from the database limited to line 1
+ *
+ * @param array $aboutMeTexts result of function to get the about me text (limited to line 1) assigned to a variable
+ *
+ * returns string of the array items concatenated with html
+ */
+function displayAboutMeQuote(array $aboutMeQuotes) : string {
     $result="";
     foreach ($aboutMeQuotes as $aboutMeQuote) {
         $result .= "<p class='contentEmphasisLine'>" . $aboutMeQuote['content'] . "<span class='contentQuote'>\"</span></p>";
---
+++ test/cms_index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: KateStarks
+ * Date: 2019-03-18
+ * Time: 15:33
+ */
