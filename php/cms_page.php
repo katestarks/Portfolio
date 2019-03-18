7731aadeda62aadb97b7422ca0c8a5f6a2524c81
katestarks <katestarks@me.com>
2019-03-18 10:09:09 +0000

    form input

--- php/cms_page.php
+++ php/cms_page.php
@@ -12,8 +12,20 @@ require_once 'dbConnection.php';
     <link href="https://fonts.googleapis.com/css?family=Quicksand:300,500" rel="stylesheet">
     <link rel='stylesheet' type='text/css' href='../css/cms_styles.css'/>
 </head>
-
 <body>
 
+    <h1>Content Managing Kate's Portfolio Page</h1>
+    <section>
+        <h2>About Me</h2>
+        <form method="post">
+            <label for="addAboutMe">Add new information or additional paragraphs here:</label>
+            <input type="text" name="addAboutMe">
+            <input type="submit">
+        </form>
+        <form method="post">
+            <label for="editAboutMe">Edit text:</label>
+            <select type=""
+        </form>
+    </section>
 </body>
 </html>
