07060a0edf5c79e33b7e1f31454000171309f30e
katestarks <katestarks@me.com>
2019-03-19 15:28:10 +0000

    unit testing for dropdown function; success, failure and malform

--- test/cms_index.php
+++ test/cms_index.php
@@ -28,27 +28,62 @@ class StackTest extends Testcase
         $this->expectException(TypeError::class);
         displayAboutMeText($aboutMeText);
     }
+
     public function testDisplayAboutMeQuoteSuccess()
     {
-        $expected = '<p>test data</p>';
-        $aboutMeText = [['content' => 'test data']];
-        $case = displayAboutMeText($aboutMeText);
+        $expected = '<p class=\'contentEmphasisLine\'>test data<span class=\'contentQuote\'>"</span></p>';
+        $aboutMeQuote = [['content' => 'test data']];
+        $case = displayAboutMeQuote($aboutMeQuote);
         $this->assertEquals($expected, $case);
     }
 
     public function testDisplayAboutMeQuoteFailure()
     {
         $expected = "";
-        $aboutMeText = [['thing'=>'words']];
-        $case = displayAboutMeText($aboutMeText);
+        $aboutMeQuote = [['thing'=>'words']];
+        $case = displayAboutMeQuote($aboutMeQuote);
         $this->assertEquals($expected, $case);
     }
 
     public function testDisplayAboutMeQuoteMalform()
     {
-        $aboutMeText = 'string';
+        $aboutMeQuote = 'string';
         $this->expectException(TypeError::class);
-        displayAboutMeText($aboutMeText);
+        displayAboutMeQuote($aboutMeQuote);
+    }
+
+//    public function testEditAboutMeTextAndQuoteSuccess()
+//    {
+//        $expected = "<option value=1>'test data'</option>";
+//        $aboutMeTextAndQuote = [];
+//        $aboutMeTextAndQuote['id'] = '1';
+//        $aboutMeTextAndQuote['content'] = 'test data';
+//        $array[0] = $aboutMeTextAndQuote;
+//        $case = editAboutMeTextAndQuote($aboutMeTextAndQuote);
+//        $this->assertEquals($expected, $case);
+//    }
+
+    public function testEditAboutMeTextAndQuoteSuccess()
+    {
+        $expected = "<option value=1>test data</option>";
+        $aboutMeTextAndQuote = [['id'=>1, 'content'=>'test data']];
+        $case = editAboutMeTextAndQuote($aboutMeTextAndQuote);
+        $this->assertEquals($expected, $case);
+    }
+
+    public function testEditAboutMeTextAndQuoteFailure()
+    {
+        $expected = '<option value=></option>';
+        $aboutMeTextAndQuote = [['thing'=>'words']];
+        $case = editAboutMeTextAndQuote($aboutMeTextAndQuote);
+        $this->assertEquals($expected, $case);
+    }
+
+    public function testEditAboutMeTextAndQuoteMalform()
+    {
+        $aboutMeTextAndQuote = 'string';
+        $this->expectException(TypeError::class);
+        editAboutMeTextAndQuote($aboutMeTextAndQuote);
     }
 }
     ?>
