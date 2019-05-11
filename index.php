b47b77029438aa13dce56bb4b1451decaaf88cab
katestarks <katestarks@me.com>
2019-05-11 13:36:14 +0100

    added JS typewriter effect; updated projects

--- index.php
+++ index.php
@@ -27,13 +27,13 @@ $displayAboutMeQuote = displayAboutMeQuote($aboutMeQuote);
         <div class="hero">
         <h1>Kate Starks {</h1>
         <p>Full Stack Developer</p>
-        <p id="heroPersonalAttr"></p>
-        <h1>}</h1>
+        <p id="heroText"></p>
+            <h1>}</h1>
         <a href="#about"><img class="heroArrow" src="IMG/heroArrow.png" alt="arrow to navigate down the site"></a>
         </div>
     </main>
     <nav>
-        <a href="login_page.php"><img class="cmsIcon" src="IMG/loginIcon.jpg" alt="small dog, link to admin login"></a>
+
         <ul>
             <li class="aboutNav"><h1><a href="#about">About</a></h1></li>
             <li class="projectsNav"><h1><a href="#projects">Projects</a></h1></li>
@@ -65,53 +65,53 @@ $displayAboutMeQuote = displayAboutMeQuote($aboutMeQuote);
         <h1>Example Projects</h1>
         <div class="project1">
             <div class="cover">
-                <h1>Responsive web page</h1>
-                <a href=""><h1>view here</h1></a>
-                <a href="https://github.com/katestarks/PilotShop"><img src="IMG/GithubLogo.png"></a>
+                <h1>Top Dog PHP App</h1>
+                <a href="http://bit.ly/topdog-nmr" target="_blank"><h1>view here</h1></a>
+                <a href="https://github.com/Mayden-Academy/2019-nmr-TopDog" target="_blank"><img src="IMG/GithubLogo.svg"></a>
             </div>
         </div>
         <div class="project2">
             <div class="cover">
-                <h1>Logo created in CSS</h1>
-                <a href=""><h1>view here</h1></a>
-                <a href=""><img src="IMG/GithubLogo.png"></a>
+                <h1>Responsive web page</h1>
+                <a href="http://dev.maydenacademy.co.uk/students/2019/feb/kate/PilotShop/" target="_blank"><h1>view here</h1></a>
+                <a href="https://github.com/katestarks/PilotShop" target="_blank"><img src="IMG/GithubLogo.svg"></a>
             </div>
         </div>
         <div class="project3">
             <div class="cover">
-                <h1>Coming Soon</h1>
-                <a href=""><h1>view here</h1></a>
-                <a href=""><img src="IMG/GithubLogo.png"></a>
+                <h1>Task Organisation PHP App</h1>
+                <a href="http://dev.maydenacademy.co.uk/students/2019/feb/kate/toDoApp/public/" target="_blank"><h1>view here</h1></a>
+                <a href="https://github.com/katestarks/toDoList" target="_blank"><img src="IMG/GithubLogo.svg"></a>
             </div>
         </div>
         <div class="project4">
             <div class="cover">
-                <h1>Coming Soon</h1>
-                <a href=""><h1>view here</h1></a>
-                <a href=""><img src="IMG/GithubLogo.png"></a>
+                <h1>Content Management System</h1>
+<!--                <a href="" target="_blank"><h1>view here</h1></a>-->
+                <a href="https://github.com/katestarks/Portfolio" target="_blank"><img src="IMG/GithubLogo.svg"></a>
             </div>
         </div>
         <div class="project5">
             <div class="cover">
-                <h1>Coming Soon</h1>
-                <a href=""><h1>view here</h1></a>
-                <a href=""><img src="IMG/GithubLogo.png"></a>
+                <h1>JavaScript Paint App</h1>
+                <a href="http://dev.maydenacademy.co.uk/projects/2019Feb/2019-paint-app/" target="_blank"><h1>view here</h1></a>
+                <a href="https://github.com/Mayden-Academy/2019-paint-app" target="_blank"><img src="IMG/GithubLogo.svg"></a>
             </div>
         </div>
         <div class="project6">
             <div class="cover">
-                <h1>Coming Soon</h1>
-                <a href=""><h1>view here</h1></a>
-                <a href=""><img src="IMG/GithubLogo.png"></a>
+                <h1>Logo created entirely in CSS</h1>
+                <a href="http://dev.maydenacademy.co.uk/students/2019/feb/kate/MaydenAcademyLogo/" target="_blank"><h1>view here</h1></a>
+                <a href="https://github.com/katestarks/AcademyLogo" target="_blank"><img src="IMG/GithubLogo.svg"></a>
             </div>
         </div>
     </section>
     <section id="contact" class="contactMe">
         <h1>Get in touch in the usual ways:</h1>
         <div>
-            <a href="mailto:katestarks@me.com"><img src="IMG/envelope.png" alt = "Envelope icon"></a>
+            <a href="mailto:kfstarks@gmail.com"><img src="IMG/envelope.png" alt = "Envelope icon"></a>
             <a href="tel:07962-071468"><img src="IMG/phone.png" alt = "Telephone icon"></a>
-            <a href="https://github.com/katestarks" target="_blank"><img src="IMG/GithubLogo.png" alt = "Github icon"></a>
+            <a href="https://github.com/katestarks" target="_blank"><img src="IMG/GithubLogo.svg" alt = "Github icon"></a>
         </div>
         <div>
             <a href="https://www.linkedin.com/in/kate-starks/" target="_blank"><img src="IMG/linkedin.png" alt = "LinkedIn icon"></a>
@@ -121,6 +121,8 @@ $displayAboutMeQuote = displayAboutMeQuote($aboutMeQuote);
         <p>I'm also happy to help with student talks; careers sessions or other ways I can get involved in the Bath, UK area.</p>
         <p>As well as coding, my favourite topics include swapping travel stories, solving dog behaviour and training issues, comparing photography portfolios or talking about behavioural economics; all areas of amateur enthusiasm for me.</p>
     </section>
-    <footer></footer>
+    <footer>
+        <a href="login_page.php"><img class="cmsIcon" src="IMG/padlock.svg" alt="padlock, link to admin login"></a>
+    </footer>
 </body>
 </html>
--- javascript/index.js
+++ javascript/index.js
@@ -1,26 +1,89 @@
-console.log('hello')
-var i = 0
-var speed = 50
-var pTag = document.getElementById('heroPersonalAttr')
 
+let i = 0
+let speed = 100
+let pTag = document.getElementById('heroText')
 
-function printlineOne () {
-    var lineOne = 'Positive | Resilient | Motivated'
+function printLineOne () {
+    let lineOne = 'Positive | Resilient | Motivated'
     if(i < lineOne.length) {
         pTag.innerHTML += lineOne.charAt(i)
         i++
-        setTimeout(printlineOne, speed)
+        setTimeout(printLineOne, speed)
     }
 }
 
-function resetTypewriter () {
-    pTag.innerHTML = ""
+function printLineTwo () {
+    let lineTwo = 'PHP | MYSQL | JavaScript | CSS | HTML'
+    if(i < lineTwo.length) {
+        pTag.innerHTML += lineTwo.charAt(i)
+        i++
+        setTimeout(printLineTwo, speed)
+    }
+}
+
+function printLineThree () {
+    let lineThree = 'Coffee Shop Owner | Dog Stalker | Wannabe Runner'
+    if(i < lineThree.length) {
+        pTag.innerHTML += lineThree.charAt(i)
+        i++
+        setTimeout(printLineThree, speed)
+    }
+}
+
+function resetTypewriterOne () {
+    pTag.innerHTML = " "
     if (i > 0) {
         i = 0
     }
-    printlineOne()
+    printLineOne()
 }
 
-printlineOne()
+function resetTypewriterTwo () {
+    pTag.innerHTML = " "
+    if (i > 0) {
+        i = 0
+    }
+    printLineTwo()
+}
 
-setInterval(resetTypewriter, 3000)
+function resetTypewriterThree () {
+    pTag.innerHTML = " "
+    if (i > 0) {
+        i = 0
+    }
+    printLineThree()
+}
+
+//line one - 32
+//line two - 37
+//line 3 - 48
+function typewriter () {
+    setTimeout(resetTypewriterOne, 0)
+    setTimeout(resetTypewriterTwo, 4000)
+    setTimeout(resetTypewriterThree, 9000)
+}
+
+typewriter()
+setInterval(typewriter, 15000)
+
+
+window.addEventListener('blur', function () {
+    console.log('blur')
+})
+
+window.addEventListener('focus', function () {
+    console.log('focus')
+})
+
+window.addEventListener('visibilityChange', function () {
+    let visibilityState = document.visibilityState
+    if (visibilityState === 'hidden') {
+        console.log('hidden')
+    } else if (visibilityState === 'visible') {
+        console.log('visible')
+    }
+})
+
+
+
+
